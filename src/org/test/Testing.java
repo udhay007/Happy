e7f0fea69e11721adeaabbf54b239b7fbdb84d5a
udhay007 <udhaysmart8@gmail.com>
2022-01-07 18:28:04 +0530

    File structure

---
+++ src/org/test/Testing.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class Testing {
+	
+	public void emplo() {
+		System.out.println("emplo");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		Testing t=new Testing();
+		t.emplo();
+		
+		
+		
+		
+	}
+
+}
